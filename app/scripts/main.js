0dad655a8272ee111e1e8dbea1468258df0a530e
RJ Goetz <rj@rjgoetz.com>
2015-10-28 14:09:28 -0500

    fixed jquery hover; small revisions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,45 @@
 $("document").ready(function () {
-timeline();
+	reconstructionImageHover();
+	jimcrowImageHover();
+	civilrightsImageHover();
+	greatmarchImageHover();
 });
 
-// rollover effect on timeline
-function timeline () {
+// rollover effect on timeline images
+function reconstructionImageHover () {
   $(".timeline-nav article:first-of-type").hover(
     function() {
       $("#timeline").prop("src", "images/timeline-reconstruction.png");
+    }, function () {
+      $("#timeline").prop("src", "images/timeline.png");
+    }
+  );  
+};
+
+function jimcrowImageHover () {
+  $(".timeline-nav article:nth-of-type(2)").hover(
+    function() {
+      $("#timeline").prop("src", "images/timeline-jim-crow.png");
+    }, function () {
+      $("#timeline").prop("src", "images/timeline.png");
+    }
+  );  
+};
+
+function civilrightsImageHover () {
+  $(".timeline-nav article:nth-of-type(3)").hover(
+    function() {
+      $("#timeline").prop("src", "images/timeline-civil.png");
+    }, function () {
+      $("#timeline").prop("src", "images/timeline.png");
+    }
+  );  
+};
+
+function greatmarchImageHover () {
+  $(".timeline-nav article:last-of-type").hover(
+    function() {
+      $("#timeline").prop("src", "images/timeline-mlk.png");
     }, function () {
       $("#timeline").prop("src", "images/timeline.png");
     }
