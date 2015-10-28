0c2ad9e30b9d0758220139a83d5e18d0c6e06d28
rjgoetz <rj@rjgoetz.com>
2015-10-28 07:02:20 -0500

    updated timeline images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$("document").ready(function () {
+timeline();
+});
+
+// rollover effect on timeline
+function timeline () {
+  $(".timeline-nav article:first-of-type").hover(
+    function() {
+      $("#timeline").prop("src", "images/timeline-reconstruction.png");
+    }, function () {
+      $("#timeline").prop("src", "images/timeline.png");
+    }
+  );  
+};
