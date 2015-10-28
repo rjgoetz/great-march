feac6325ebfcb8bbd56af0f4ad38538d6370478f
RJ Goetz <rj@rjgoetz.com>
2015-10-28 15:16:38 -0500

    added subnav jquery; updated subnav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,25 @@
 $("document").ready(function () {
 	reconstructionImageHover();
-	jimcrowImageHover();
-	civilrightsImageHover();
-	greatmarchImageHover();
+	reconstructionSubNavHover();
+	jimCrowImageHover();
+	jimCrowSubNavHover();
+	civilRightsImageHover();
+	civilRightsSubNavHover();
+	greatMarchImageHover();
 });
 
 // rollover effect on timeline images
-function reconstructionImageHover () {
-  $(".timeline-nav article:first-of-type").hover(
-    function() {
-      $("#timeline").prop("src", "images/timeline-reconstruction.png");
-    }, function () {
-      $("#timeline").prop("src", "images/timeline.png");
+function reconstructionImageHover() {
+	$(".timeline-nav article:first-of-type").hover(
+	  function() {
+		$("#timeline").prop("src", "images/timeline-reconstruction.png");
+	  }, function () {
+		$("#timeline").prop("src", "images/timeline.png");
     }
   );  
 };
 
-function jimcrowImageHover () {
+function jimCrowImageHover() {
   $(".timeline-nav article:nth-of-type(2)").hover(
     function() {
       $("#timeline").prop("src", "images/timeline-jim-crow.png");
@@ -26,7 +29,7 @@ function jimcrowImageHover () {
   );  
 };
 
-function civilrightsImageHover () {
+function civilRightsImageHover() {
   $(".timeline-nav article:nth-of-type(3)").hover(
     function() {
       $("#timeline").prop("src", "images/timeline-civil.png");
@@ -36,12 +39,64 @@ function civilrightsImageHover () {
   );  
 };
 
-function greatmarchImageHover () {
+function greatMarchImageHover() {
   $(".timeline-nav article:last-of-type").hover(
     function() {
       $("#timeline").prop("src", "images/timeline-mlk.png");
     }, function () {
       $("#timeline").prop("src", "images/timeline.png");
+    }
+  );  
+};
+
+//rollover image swap on subnav
+function reconstructionSubNavHover() {
+	$(".timeline-nav .period-img:first-of-type .period-subnav li:nth-of-type(2) a").hover(
+    function() {
+    	$("#reconstruction-nav").prop("src", "images/jim-crow.jpg");
+    }, function () {
+      $("#reconstruction-nav").prop("src", "images/abe-lincoln.jpg");
+    }
+  );
+	$(".timeline-nav .period-img:first-of-type .period-subnav li:nth-of-type(3) a").hover(
+    function() {
+    	$("#reconstruction-nav").prop("src", "images/jim-crow.jpg");
+    }, function () {
+      $("#reconstruction-nav").prop("src", "images/abe-lincoln.jpg");
+    }
+  );  
+};
+
+function jimCrowSubNavHover() {
+	$(".timeline-nav .period-img:nth-of-type(2) .period-subnav li:nth-of-type(2) a").hover(
+    function() {
+    	$("#jim-crow-nav").prop("src", "images/abe-lincoln.jpg");
+    }, function () {
+      $("#jim-crow-nav").prop("src", "images/jim-crow.jpg");
+    }
+  );
+	$(".timeline-nav .period-img:nth-of-type(2) .period-subnav li:nth-of-type(3) a").hover(
+    function() {
+    	$("#jim-crow-nav").prop("src", "images/abe-lincoln.jpg");
+    }, function () {
+      $("#jim-crow-nav").prop("src", "images/jim-crow.jpg");
+    }
+  );  
+};
+
+function civilRightsSubNavHover() {
+	$(".timeline-nav .period-img:nth-of-type(3) .period-subnav li:nth-of-type(2) a").hover(
+    function() {
+    	$("#civil-rights-nav").prop("src", "images/jim-crow.jpg");
+    }, function () {
+      $("#civil-rights-nav").prop("src", "images/brown-vs-board-of-education.jpg");
+    }
+  );
+	$(".timeline-nav .period-img:nth-of-type(3) .period-subnav li:nth-of-type(3) a").hover(
+    function() {
+    	$("#civil-rights-nav").prop("src", "images/jim-crow.jpg");
+    }, function () {
+      $("#civil-rights-nav").prop("src", "images/brown-vs-board-of-education.jpg");
     }
   );  
 };
