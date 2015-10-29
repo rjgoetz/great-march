1481921c82a8dca122e5b38dcbd8353328f39293
rjgoetz <rj@rjgoetz.com>
2015-10-28 21:10:51 -0500

    added favicon; added home images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,7 @@ function reconstructionImageHover() {
 	  }, function () {
 		$("#timeline").prop("src", "images/timeline.png");
     }
-  );  
+  );
 };
 
 function jimCrowImageHover() {
@@ -26,7 +26,7 @@ function jimCrowImageHover() {
     }, function () {
       $("#timeline").prop("src", "images/timeline.png");
     }
-  );  
+  );
 };
 
 function civilRightsImageHover() {
@@ -36,7 +36,7 @@ function civilRightsImageHover() {
     }, function () {
       $("#timeline").prop("src", "images/timeline.png");
     }
-  );  
+  );
 };
 
 function greatMarchImageHover() {
@@ -46,57 +46,57 @@ function greatMarchImageHover() {
     }, function () {
       $("#timeline").prop("src", "images/timeline.png");
     }
-  );  
+  );
 };
 
 //rollover image swap on subnav
 function reconstructionSubNavHover() {
 	$(".timeline-nav .period-img:first-of-type .period-subnav li:nth-of-type(2) a").hover(
     function() {
-    	$("#reconstruction-nav").prop("src", "images/jim-crow.jpg");
+    	$("#reconstruction-nav").prop("src", "images/black-codes.jpg");
     }, function () {
       $("#reconstruction-nav").prop("src", "images/abe-lincoln.jpg");
     }
   );
 	$(".timeline-nav .period-img:first-of-type .period-subnav li:nth-of-type(3) a").hover(
     function() {
-    	$("#reconstruction-nav").prop("src", "images/jim-crow.jpg");
+    	$("#reconstruction-nav").prop("src", "images/fifteenth-amendment.jpg");
     }, function () {
       $("#reconstruction-nav").prop("src", "images/abe-lincoln.jpg");
     }
-  );  
+  );
 };
 
 function jimCrowSubNavHover() {
 	$(".timeline-nav .period-img:nth-of-type(2) .period-subnav li:nth-of-type(2) a").hover(
     function() {
-    	$("#jim-crow-nav").prop("src", "images/abe-lincoln.jpg");
+    	$("#jim-crow-nav").prop("src", "images/plessy-v-ferguson.jpg");
     }, function () {
-      $("#jim-crow-nav").prop("src", "images/jim-crow.jpg");
+      $("#jim-crow-nav").prop("src", "images/disfranchisement.jpg");
     }
   );
 	$(".timeline-nav .period-img:nth-of-type(2) .period-subnav li:nth-of-type(3) a").hover(
     function() {
-    	$("#jim-crow-nav").prop("src", "images/abe-lincoln.jpg");
+    	$("#jim-crow-nav").prop("src", "images/jim-crow.jpg");
     }, function () {
-      $("#jim-crow-nav").prop("src", "images/jim-crow.jpg");
+      $("#jim-crow-nav").prop("src", "images/disfranchisement.jpg");
     }
-  );  
+  );
 };
 
 function civilRightsSubNavHover() {
 	$(".timeline-nav .period-img:nth-of-type(3) .period-subnav li:nth-of-type(2) a").hover(
     function() {
-    	$("#civil-rights-nav").prop("src", "images/jim-crow.jpg");
+    	$("#civil-rights-nav").prop("src", "images/kkk.jpg");
     }, function () {
       $("#civil-rights-nav").prop("src", "images/brown-vs-board-of-education.jpg");
     }
   );
 	$(".timeline-nav .period-img:nth-of-type(3) .period-subnav li:nth-of-type(3) a").hover(
     function() {
-    	$("#civil-rights-nav").prop("src", "images/jim-crow.jpg");
+    	$("#civil-rights-nav").prop("src", "images/birmingham-riots.jpg");
     }, function () {
       $("#civil-rights-nav").prop("src", "images/brown-vs-board-of-education.jpg");
     }
-  );  
+  );
 };
